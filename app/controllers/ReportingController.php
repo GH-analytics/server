141af57ddb9d782533dc16aedacf7ea98cc8df7a
Michael Schuett <michaeljs1990@gmail.com>
2014-10-22 23:27:13 -0500

    unoptimized word sorting function.

--- app/controllers/ReportingController.php
+++ app/controllers/ReportingController.php
@@ -8,13 +8,35 @@ class ReportingController extends ApiController {
      * @param $id
      */
     public function userWordCloud($participant_id, $start_date = null, $end_date = null) {
-        // Get all participant data specified.
-        $messages = Message::where('participant_id', '=', $participant_id)->get();
+        set_time_limit(200);
         
-        if(is_object($messages)) {
-            foreach($messages->toArray() as $message) {
-                dd($message['message']);
+        $stats = []; // make an empty array to store data in.
+        $skip = 0; // How many rows to skip before getting new data
+        $take = 1000; // How many to get at a time
+        $messages = Message::where('participant_id', '=', $participant_id)->take($take)->lists('message');
+
+        if(count($messages)) {
+            // Loop till we have gone through all the messages.
+            while(count($messages)) {
+                // Loop over each message returned.
+                foreach ($messages as $message) {
+                    // Loop over each word in each messages
+                    foreach(explode(' ', $message) as $word) {
+                        if(!isset($stats[$word])) $stats[$word] = 1;
+                        else $stats[$word]++;
+                    }
+                }
+
+                // Increment the data we would like to grab.
+                $skip += 1000;
+                $take += 1000;
+                $messages = Message::where('participant_id', '=', $participant_id)->skip($skip)->take($take)->get();
             }
+
+            arsort($stats);
+            dd($stats);
+        } else {
+            return $this->respondConflict("No messages for this user");
         }
     }
 
